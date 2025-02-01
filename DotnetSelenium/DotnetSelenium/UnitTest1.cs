7fe4ec0d1f20bb0030d77a5e05eee9f0635417db
Artur Fidelis <artur.fidelis@signainfo.com.br>
2025-02-01 00:08:54 -0300

    Selenium with C# .NET (.NET 8 and C# 12)

---
+++ DotnetSelenium/DotnetSelenium/UnitTest1.cs
@@ -0,0 +1,35 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace DotnetSelenium
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            // Sudo code for setting up Selenium
+            // 1. Create a new instance of Selenium Web Driver
+            IWebDriver driver = new ChromeDriver();
+
+            // 2. Navigate to the URL
+            driver.Navigate().GoToUrl("https://www.google.com/");
+            // 2a. Maximize the browser window
+            driver.Manage().Window.Maximize();
+
+            // 3. Find the element
+            IWebElement webElement = driver.FindElement(By.Name("q"));
+
+            // 4. Type in the element
+            webElement.SendKeys("Selenium");
+
+            //5. Click on the element
+            webElement.SendKeys(Keys.Return);
+        }
+    }
+}
