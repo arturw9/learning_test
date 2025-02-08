ca34aa8a7fd7c2c6e14e35d9343f51f518703099
artursigna <artur.fidelis@signainfo.com.br>
2025-02-08 15:39:44 -0300

    up

--- DotnetSelenium/DotnetSelenium/UnitTest1.cs
+++ DotnetSelenium/DotnetSelenium/UnitTest1.cs
@@ -51,6 +51,37 @@ namespace DotnetSelenium
             IWebElement txtUserName = driver.FindElement(By.Name("UserName"));
             // 6. Type on the textUserName
             txtUserName.SendKeys("admin");
+            // 7. Find the Password text box
+            IWebElement txtPassword = driver.FindElement(By.Id("Password"));
+            // 8. Type on the textUserName
+            txtPassword.SendKeys("password");
+            // 9. Identify the Login Button using Class Name
+            //IWebElement btnLogin = driver.FindElement(By.ClassName("btn"));
+            // 9. Identify the Login Button using CssSelector
+            IWebElement btnLogin = driver.FindElement(By.CssSelector(".btn"));
+            // 10. Click login button
+            btnLogin.Submit();
+        }
+
+        [Test]
+        public void EAWebSiteTestReduceSizeCode()
+        {
+            // Sudo code for setting up Selenium
+            // 1. Create a new instance of Selenium Web Driver
+            IWebDriver driver = new ChromeDriver();
+
+            // 2. Navigate to the URL
+            driver.Navigate().GoToUrl("http://eaapp.somee.com/");
+            // 3. Find and Click the login link
+            driver.FindElement(By.Id("loginLink")).Click();
+            // 5. Find the UserName text box
+            driver.FindElement(By.Name("UserName")).SendKeys("admin");
+            // 7. Find the Password text box
+            driver.FindElement(By.Id("Password")).SendKeys("password");
+            // 9. Identify the Login Button using Class Name
+            //IWebElement btnLogin = driver.FindElement(By.ClassName("btn"));
+            // 9. Identify the Login Button using CssSelector
+            driver.FindElement(By.CssSelector(".btn")).Submit();
         }
     }
 }
